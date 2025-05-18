ccc07e2eea8b14906af95800b6152bbe36f84ab3
shubham88fru <shubham88fru@yahoo.com>
2025-05-18 15:39:48 -0700

    user form component

---
+++ users/src/UserForm.js
@@ -0,0 +1,15 @@
+export default function UserForm() {
+    return <form>
+        <div>
+            <label>Name</label>
+            <input />
+        </div>
+
+        <div>
+            <label>Email</label>
+            <input />
+        </div>
+
+        <button>Add User</button>
+    </form>
+}
