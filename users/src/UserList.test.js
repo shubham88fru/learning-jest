2d483c3b08879e2aa3d360aa15299b369c16d78a
shubham88fru <shubham88fru@yahoo.com>
2025-05-24 15:52:04 -0700

    refactor

--- users/src/UserList.test.js
+++ users/src/UserList.test.js
@@ -1,23 +1,37 @@
 import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
-test("render the correct number of rows", () => {
-  // render the component
+function renderComponent() {
   const users = [
-    { name: "Shubham", email: "test@test.com" },
-    { name: "wolverine", email: "wolverine@marvel.com" },
+    { name: "jane", email: "jane@jane.com" },
+    { name: "sam", email: "sam@sam.com" },
   ];
 
-  const { container } = render(<UserList users={users} />);
+  return { rndr: render(<UserList users={users} />), users };
+}
+
+test("render one row per user", () => {
+  // render the component
+  const { rndr } = renderComponent();
 
   //find all the rows in the table
   // const rows = within(screen.getByTestId("users")).getAllByRole("row");
 
   // eslint-disable-next-line
-  const rows = container.querySelectorAll("tbody tr");
+  const rows = rndr.container.querySelectorAll("tbody tr");
 
   //assertions: correct number of rows in the table.
   expect(rows).toHaveLength(2);
 });
 
-test("render the email and name of each user", () => {});
+test("render the email and name of each user", () => {
+  const { users } = renderComponent();
+
+  for (let user of users) {
+    const name = screen.getByRole("cell", { name: user.name });
+    const email = screen.getByRole("cell", { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
+});
