8f09cbfbbaafa7953793bebd8c9b47c4687897bc
shubham88fru <shubham88fru@yahoo.com>
2025-05-25 13:10:43 -0700

    add test

--- users/src/App.test.js
+++ users/src/App.test.js
@@ -1,8 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import user from "@testing-library/user-event";
 
-test("renders learn react link", () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+test("can receive a new user and display it on the list", () => {
+  render(<App />);
+
+  const nameInput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
+  const button = screen.getByRole("button");
+
+  user.click(nameInput);
+  user.keyboard("shubham");
+  user.click(emailInput);
+  user.keyboard("test@test.com");
+
+  user.click(button);
+
+  const name = screen.getByRole("cell", { name: "shubham" });
+  const email = screen.getByRole("cell", { name: "test@test.com" });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
 });
