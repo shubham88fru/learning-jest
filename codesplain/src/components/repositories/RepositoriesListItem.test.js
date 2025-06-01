3ed44c8ffa88bffba91479df6361dabddb193ffb
shubham88fru <shubham88fru@yahoo.com>
2025-06-01 13:59:41 -0700

    add more tests

--- codesplain/src/components/repositories/RepositoriesListItem.test.js
+++ codesplain/src/components/repositories/RepositoriesListItem.test.js
@@ -7,7 +7,9 @@ function renderComponent() {
     full_name: "facebook/react",
     language: "JavaScript",
     description: "A JS library",
-    owner: "facebook",
+    owner: {
+      login: "facebook",
+    },
     name: "react",
     html_url: "https://githubh.com/facebook/react",
   };
@@ -17,10 +19,37 @@ function renderComponent() {
       <RepositoriesListItem repository={repository} />
     </MemoryRouter>
   );
+
+  return { repository };
 }
 
 test("shows a link to the repository", async () => {
-  renderComponent();
+  const { repository } = renderComponent();
 
   await screen.findByRole("img", { name: "JavaScript" });
+
+  const link = screen.getByRole("link", {
+    name: /github repository/i,
+  });
+  expect(link).toHaveAttribute("href", repository.html_url);
+});
+
+test("shows a file icond with appropriate icon", async () => {
+  renderComponent();
+
+  const icon = await screen.findByRole("img", { name: "JavaScript" });
+
+  expect(icon).toHaveClass("js-icon");
+});
+
+test("shows a link to the code editor page", async () => {
+  const { repository } = renderComponent();
+
+  await screen.findByRole("img", { name: "JavaScript" });
+
+  const link = await screen.findByRole("link", {
+    name: new RegExp(repository.owner.login, "i"),
+  });
+
+  expect(link).toHaveAttribute("href", `/repositories/${repository.full_name}`);
 });
