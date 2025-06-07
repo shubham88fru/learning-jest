0a916d6258879648f0c45ebfeaa35c11591711c3
shubham88fru <shubham88fru@yahoo.com>
2025-06-07 16:57:05 -0700

    remove unused imports

--- codesplain/src/routes/HomeRoutes.test.js
+++ codesplain/src/routes/HomeRoutes.test.js
@@ -1,6 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import { setupServer } from "msw/node";
-import { rest } from "msw";
 import { MemoryRouter } from "react-router-dom";
 import HomeRoute from "./HomeRoute";
 import { createServer } from "../test/server";
