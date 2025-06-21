3f2526cf924561bddb4557412a8b41aaec6c4479
shubham88fru <shubham88fru@yahoo.com>
2025-06-21 16:15:04 -0500

    test scenario when user is not signed in

---
+++ codesplain/src/components/auth/AuthButtons.test.js
@@ -0,0 +1,70 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import { createServer } from "../../test/server";
+import AuthButtons from "./AuthButtons";
+
+async function renderComponent() {
+  render(
+    <MemoryRouter>
+      <AuthButtons />
+    </MemoryRouter>
+  );
+  await screen.findAllByRole("link");
+}
+
+describe("when user is not signed in", () => {
+  // createServer() --> GET '/api/user' --> { user: null }
+  createServer([
+    {
+      path: "/api/user",
+      res: () => {
+        return { user: null };
+      },
+    },
+  ]);
+
+  test("sign in and singup are visible", async () => {
+    await renderComponent();
+
+    const singInButton = screen.getByRole("link", { name: /sign in/i });
+    const signUpButton = screen.getByRole("link", { name: /sign up/i });
+
+    expect(singInButton).toBeInTheDocument();
+    expect(singInButton).toHaveAttribute("href", "/signin");
+    expect(signUpButton).toBeInTheDocument();
+    expect(signUpButton).toHaveAttribute("href", "/signup");
+  });
+
+  test("sign out is not visible", async () => {
+    await renderComponent();
+    const signOutButton = screen.queryByRole("link", { name: /sign out/i });
+    expect(signOutButton).not.toBeInTheDocument();
+  });
+});
+
+describe("when user is signed in", () => {
+  createServer([
+    {
+      path: "/api/user",
+      res: () => {
+        return { user: { id: 3, email: "email@email.com" } };
+      },
+    },
+  ]);
+
+  // createServer() --> GET '/api/user' --> { user: { id: 3, email: 'email@email.com' } }
+  test("sign in and sign up are not visible", async () => {
+    await renderComponent();
+    // const signOutButton = screen.queryByRole("link", { name: /sign out/i });
+    // const signInButton = screen.queryByRole("link", { name: /sign in/i });
+    // expect(signInButton).not.toBeInTheDocument();
+    // expect(signOutButton).not.toBeInTheDocument();
+  });
+
+  test("sign out is visible", async () => {
+    await renderComponent();
+    // const signOutButton = screen.getByRole("link", { name: /sign out/i });
+    // expect(signOutButton).toBeInTheDocument();
+    // expect(signOutButton).toHaveAttribute("href", "/signout");
+  });
+});
